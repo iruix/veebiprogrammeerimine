eae1b0d1ce08334a6a417fcf0d6a3b022db3c7b3
Braian Jullinen <iruix@users.noreply.github.com>
2018-09-10 10:40:52 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Long live X</title>
+</head>
+<body>
+  <h1>X</h1>
+  <p> <a  href="https://www.wincalendar.com/Calendar/Date/June-18-2018" target="_blank">June 18, 2018</a></p>
+  <!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra Õppehoone">-->
+  <!-- see tähendab, et ei loe faili. Target="_blank" näitab, et avab uues tabis lingi. see lõpetab selle kommentaari --> 
+    <img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra Õppehoone">
+	<p>Mu naaber teeb ka veebi, siin on ta <a href="http://greeny.cs.tlu.ee/~alekmog/" target="_blank">leht</a></p>
+</body>
+</html>
